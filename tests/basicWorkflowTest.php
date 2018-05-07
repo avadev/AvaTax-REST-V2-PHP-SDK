eab4bfa261f1a4cc6f588a456631cc52fc235753
HAN BAO <hbao2016@hotmail.com>
2018-05-07 15:42:04 -0700

    put back exempt line

--- tests/basicWorkflowTest.php
+++ tests/basicWorkflowTest.php
@@ -51,6 +51,7 @@ final class AvaTaxClientTest extends TestCase
             ->withAddress('ShipTo', '100 Ravine Lane', null, null, 'Bainbridge Island', 'WA', '98110', 'US')
             ->withLine(100.0, 1, null, "P0000000")
             ->withLine(1234.56, 1, null, "P0000000")
+            ->withExemptLine(50.0, null, "NT")
             ->withLine(2000.0, 1, null, "P0000000")
             ->withLineAddress(Avalara\TransactionAddressType::C_SHIPFROM, "123 Main Street", null, null, "Irvine", "CA", "92615", "US")
             ->withLineAddress(Avalara\TransactionAddressType::C_SHIPTO, "1500 Broadway", null, null, "New York", "NY", "10019", "US")
