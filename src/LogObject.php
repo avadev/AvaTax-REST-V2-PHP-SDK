290f0608d47a4dbb0c15d7289204fa4619ba9d27
Niraj Kumar Chauhan <nirajkumar.chauhan@learningmate.com>
2024-10-06 15:48:21 +0530

    (fix) - formatting

--- src/LogObject.php
+++ src/LogObject.php
@@ -17,59 +17,61 @@ class LogInformation
 
     public function setStartTime($startTime)
     {
-        $this->startTime = $startTime;
+        $this-> startTime = $startTime;
     }
 
     public function populateRequestInfo($verb, $apiUrl, $guzzleParams, $logRequestAndResponseBody)
     {
-        $this->timestamp = gmdate("Y/m/d H:i:s");
-        $this->httpMethod = $verb;
-        $this->requestURI = $apiUrl;
-        $this->logRequestAndResponseBody = $logRequestAndResponseBody;
-        if ($this->logRequestAndResponseBody) {
-            $this->requestDetails = $guzzleParams['body'];
+        $this-> timestamp = gmdate("Y/m/d H:i:s");
+        $this-> httpMethod = $verb;
+        $this-> requestURI = $apiUrl;
+        $this-> logRequestAndResponseBody = $logRequestAndResponseBody;
+        if($this-> logRequestAndResponseBody)
+        {
+            $this-> requestDetails = $guzzleParams['body'];
         }
     }
 
     public function populateResponseInfo($jsonBody, $response)
     {
-        $this->populateCommonResponseInfo($response);
-        if ($this->logRequestAndResponseBody) {
-            $this->responseDetails = $jsonBody;
+        $this-> populateCommonResponseInfo($response);
+        if($this-> logRequestAndResponseBody)
+        {
+            $this-> responseDetails = $jsonBody;
         }
     }
 
-    public function populateErrorInfoWithMessageAndBody($errorMessage, $response)
+    public function populateErrorInfoWithMessageAndBody($errorMessage, $response) 
     {
-        $this->populateCommonResponseInfo($response);
-        $this->exceptionMessage = $errorMessage;
-        $this->statusCode = 500;
+        $this-> populateCommonResponseInfo($response);
+        $this-> exceptionMessage = $errorMessage;
+        $this-> statusCode = 500;
     }
 
     public function populateErrorInfo($e, $errorContents)
     {
-        $this->populateTotalExecutionTime();
-        $this->statusCode = $e->getCode();
-        if ($e->getResponse()->hasHeader('x-correlation-id')) {
-            $this->headerCorrelationId = $e->getResponse()->getHeader('x-correlation-id')[0];
+        $this-> populateTotalExecutionTime();
+        $this-> statusCode = $e-> getCode();
+        if($e-> getResponse()-> hasHeader('x-correlation-id')) {
+            $this-> headerCorrelationId = $e-> getResponse()-> getHeader('x-correlation-id')[0];
         }
-        $this->exceptionMessage = $errorContents;
+        $this-> exceptionMessage = $errorContents; 
     }
 
     private function populateCommonResponseInfo($response)
     {
-        $this->populateTotalExecutionTime();
-        if ($response->hasHeader('x-correlation-id')) {
-            $this->headerCorrelationId = $response->getHeader('x-correlation-id')[0];
+        $this-> populateTotalExecutionTime();
+        if($e-> getResponse()-> hasHeader('x-correlation-id')) {
+            $this-> headerCorrelationId = $response-> getHeader('x-correlation-id')[0];
         }
-        $this->statusCode = $response->getStatusCode();
+        $this-> statusCode = $response-> getStatusCode();
     }
 
-    private function populateTotalExecutionTime()
+    private function populateTotalExecutionTime() 
     {
-        $time_elapsed_secs = microtime(true) - $this->startTime;
+        $time_elapsed_secs = microtime(true) - $this-> startTime;
         $milliseconds = round($time_elapsed_secs * 1000);
-        $this->totalExecutionTime = $milliseconds;
-    }
+        $this-> totalExecutionTime = $milliseconds; 
+    }    
 }
 ?>
