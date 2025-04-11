1933664c8f9f6955f18a2f659e073b4a7d240804
svc-developer <svc-developer@avalara.com>
2025-04-11 15:36:58 +0000

    25.4.0

--- src/Constants.php
+++ src/Constants.php
@@ -2,7 +2,7 @@
 namespace Avalara;
 use GuzzleHttp\Client;
 
-define('AVATAX_SDK_VERSION', '25.3.2');
+define('AVATAX_SDK_VERSION', '25.4.0');
 
 /*****************************************************************************
  *                                                                           *
